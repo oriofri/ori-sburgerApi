7d1b88fa06ad05f7f60a95c4a216f85bcd28af60
ori ofri <ofri.ori@gmail.com>
2022-08-22 20:23:58 +0300

    mydeploy

--- burgerapi/settings.py
+++ burgerapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@m9w4i^po@h96yw#lj#@&t6m92$s7dt8=#a$=mi1@)s3qyca4h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG',default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
